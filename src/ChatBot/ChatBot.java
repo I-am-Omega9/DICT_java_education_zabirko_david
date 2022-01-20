aa51a4df8e75de44bb5bc98c24bda5e091604204
I-am-Omega9 <d.v.zabirko@student.khai.edu>
2022-01-20 19:20:51 +0300

    ChatBot 2-nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,8 +1,17 @@
 package ChatBot;
 
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
         System.out.println("Hi! My name is 'The expelled one'!");
-        System.out.println("I was created in 2021");
+        System.out.println("I was created in 2021.");
+        System.out.println("Please, remind me your name.");
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+
+        System.out.printf("What a great name you have, %s", name);
+        System.out.print("!");
+        in.close();
     }
 }
