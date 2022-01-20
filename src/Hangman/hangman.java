24d177176bb6068a5b1accd65f68c725973580d1
I-am-Omega9 <d.v.zabirko@student.khai.edu>
2022-01-20 21:02:40 +0300

    Hangman 3-rd stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -1,16 +1,19 @@
 package Hangman;
 import java.util.Scanner;
+import java.util.Random;
 
 class Hangman {
     public static void main(String[] args) {
-        String word = "java";
+        String[] words = {"java", "kotlin", "python", "javascript"};
+        Random randomIntGen = new Random();
+        int randomInt = randomIntGen.nextInt(words.length);
+        String word = words[randomInt];
         System.out.println("HANGMAN \nGuess the word:");
         Scanner input = new Scanner(System.in);
         String answer = input.nextLine();
         if (answer.equals(word)) {
             System.out.println("You survived!");
-        }
-        else {
+        } else {
             System.out.println("You lost!");
         }
     }
