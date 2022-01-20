1126e7a0bfd5633924d0b511302135f380b70caf
I-am-Omega9 <d.v.zabirko@student.khai.edu>
2022-01-20 20:36:54 +0300

    Hangman 2-nd stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -1,7 +1,17 @@
 package Hangman;
+import java.util.Scanner;
 
-public class hangman {
+class Hangman {
     public static void main(String[] args) {
-        System.out.println("HANGMAN \nThe game will be available soon.");
+        String word = "java";
+        System.out.println("HANGMAN \nGuess the word:");
+        Scanner input = new Scanner(System.in);
+        String answer = input.nextLine();
+        if (answer.equals(word)) {
+            System.out.println("You survived!");
+        }
+        else {
+            System.out.println("You lost!");
+        }
     }
 }
